786e417d994bca46e3e4162fb1a449ef8922b411
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2022-09-17 09:26:39 +0200

    KLIP170922REV1
    Database script updated

--- src/main/java/com/jakubwawak/database/Database_Connector.java
+++ src/main/java/com/jakubwawak/database/Database_Connector.java
@@ -5,9 +5,20 @@
  */
 package com.jakubwawak.database;
 
+import com.jakubwawak.maintanance.Configuration_Service;
+
 /**
  * Object for connecting to database
  */
 
 public class Database_Connector {
+    Configuration_Service cs;
+
+    /**
+     * Constructor
+     * @param cs
+     */
+    public Database_Connector(Configuration_Service cs){
+        this.cs = cs;
+    }
 }
