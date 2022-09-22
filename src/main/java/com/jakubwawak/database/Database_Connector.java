f98a44af0dbf2985ab6b23f593300847df0b4133
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2022-09-22 13:45:26 +0200

    KLIP220922REV1

--- src/main/java/com/jakubwawak/database/Database_Connector.java
+++ src/main/java/com/jakubwawak/database/Database_Connector.java
@@ -6,6 +6,13 @@
 package com.jakubwawak.database;
 
 import com.jakubwawak.maintanance.Configuration_Service;
+import com.jakubwawak.maintanance.LoGrabber;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 /**
  * Object for connecting to database
@@ -13,12 +20,49 @@ import com.jakubwawak.maintanance.Configuration_Service;
 
 public class Database_Connector {
     Configuration_Service cs;
+    LoGrabber nl;
+    String logged_user_login;
+
+    public boolean connected;
+    public LocalDateTime run_time;
+
+    public Connection con;
+
 
     /**
      * Constructor
      * @param cs
      */
-    public Database_Connector(Configuration_Service cs){
+    public Database_Connector(Configuration_Service cs, LoGrabber nl){
         this.cs = cs;
+        this.nl = nl;
+        logged_user_login = "";
     }
+
+    /**
+     * Function for connecting service to database
+     */
+    public void connect(){
+        if ( !cs.empty() ){
+            nl.add("Trying to connect to database with user: "+cs.klip_raw_database_user,"DATABASE-CONNECTOR");
+            String login_data = "jdbc:mysql://"+cs.klip_raw_database+"/"+cs.klip_raw_database_name+"?"
+                    + "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&" +
+                    "user="+cs.klip_raw_database_user+"&password="+cs.klip_raw_database_password;
+            try{
+                con = DriverManager.getConnection(login_data);
+                run_time = LocalDateTime.now( ZoneId.of( "Europe/Warsaw" ) );
+                nl.add("Connected succesfully","CONNECTION");
+                nl.add(login_data.substring(0,login_data.length()-25)+"...*END*","CONNECTION");
+                connected = true;
+            }catch(SQLException e){
+                connected = false;
+                nl.add("Failed to connect to database ("+e.toString()+")","ERROR-DB01");
+            }
+        }
+        else{
+            nl.add("Failed to connect, configuration file is empty.","DATABASE-CONFIGURATION-EMPTY");
+        }
+    }
+
+
 }
--- src/main/java/com/jakubwawak/klipAPI/KlipApiApplication.java
+++ src/main/java/com/jakubwawak/klipAPI/KlipApiApplication.java
@@ -5,8 +5,12 @@
  */
 package com.jakubwawak.klipAPI;
 
+import com.jakubwawak.maintanance.Configuration_Service;
+import com.jakubwawak.maintanance.LoGrabber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.module.Configuration;
 
 /**
  * Main application object
@@ -15,7 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KlipApiApplication {
 
 	public static String version =  "v1.0.0";
-	public static String build = "KLIP170922REV1";
+	public static String build = "KLIP220922REV1";
 
 	/**
 	 * Main application function
@@ -23,8 +27,36 @@ public class KlipApiApplication {
 	 */
 	public static void main(String[] args) {
 		show_header();
-		SpringApplication.run(KlipApiApplication.class, args);
+		LoGrabber log = new LoGrabber("klipapi_");
+		Configuration_Service cs = new Configuration_Service(log,build);
+		if ( cs.configuration_file_exists ){
+			cs.load_config();
+			//configuration exists - loaded data
+			//api can start
+			run(args);
+		}
+		else{
+			// need to load data from user - configuration file not exists
+			log.add("Loading configuration data from user...","CONFIG-FILE-NOTFOUND");
+			try{
+				cs.user_load_config();
+				cs.create_config("config_new.nami");
+				cs.load_config();
+				//configutarion ready - loaded from user
+				//api can start
+				run(args);
+			}catch(Exception e){
+				log.add("Major error ("+e.toString()+")","MAJOR-API-ERROR");
+			}
+		}
+	}
 
+	/**
+	 * Function for running main program
+	 * @param args
+	 */
+	public static void run(String[] args){
+		SpringApplication.run(KlipApiApplication.class, args);
 	}
 	/**
 	 * Function for showing application header
